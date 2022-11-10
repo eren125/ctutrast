09bab58b2a96364d76f4af9f8586f8475bbc5b5d
eren125 <emmanuelren1995@gmail.com>
2022-11-10 19:03:58 +0100

    channel dimension 1st approach to improve

--- src/tutrast.cpp
+++ src/tutrast.cpp
@@ -31,6 +31,7 @@ double grid_calc_enthalpy(gemmi::Grid<double> grid, double energy_threshold, dou
 }
 
 string channel_dim(uint16_t* cc_labels, size_t label, size_t nu, size_t nv, size_t nw) {
+  // TODO try and replace slice loop by random draws without replacement
   string channels;
   bool present; // checks if a slice contains the label
   // YZ slicing
@@ -48,6 +49,7 @@ string channel_dim(uint16_t* cc_labels, size_t label, size_t nu, size_t nv, size
     if (present == false) {break;}
   }
   if (present == true) {channels += 'X';}
+  // XZ slicing
   for (size_t v = 0; v < nv; v++){
     present = false;
     while (present == false) {
@@ -62,6 +64,7 @@ string channel_dim(uint16_t* cc_labels, size_t label, size_t nu, size_t nv, size
     if (present == false) {break;}
   }
   if (present == true) {channels += 'Y';}
+  // XY slicing
   for (size_t w = 0; w < nw; w++){
     present = false; //if label is in slice
     while (present == false) {
@@ -86,38 +89,59 @@ int main(int argc, char* argv[]) {
   double temperature = stod(argv[2]);
   double energy_threshold = stod(argv[3]); //kJ/mol
   gemmi::Ccp4<double> map;
-  map.read_ccp4_file(grid_file);
-  // double grid_min = map.hstats.dmin;
-
-  // Set up a binary 3D array (is channel)
+  // READ MAP that took about 870 ms to make for AEI
+  // (AEI time = 29 ms)
+  map.read_ccp4_file(grid_file); 
+  double grid_min = map.hstats.dmin;
+  // Set up arrays of 0 (if framework) 1 (if void)
+  // (AEI time = 11 ms)
   size_t array_size = map.grid.nu*map.grid.nv*map.grid.nw;
   int* channel_labels = new int[array_size](); 
   size_t idx = 0;
-  for (size_t i=0; i<array_size; i++){
+  for (size_t i=0; i<array_size; i++){ 
     if (map.grid.data[i] < energy_threshold){
       channel_labels[i] = 1;
-      // map.grid.data[i]=-40;
     }
   }
   size_t N = 0;
-  uint16_t* channel_cc_labels = cc3d::connected_components3d<int, uint16_t>(
-  channel_labels, /*sx=*/map.grid.nu, /*sy=*/map.grid.nv, /*sz=*/map.grid.nw, /*connectivity=*/7, /*N=*/N );
+  // Channel labellisation using a 3D connected component algorithm modified to integrate PBC
+  // (AEI time = 14ms)
+  uint16_t* channel_cc_labels = cc3d::connected_components3d<int, uint16_t, true>(
+  channel_labels, /*sx=*/map.grid.nu, /*sy=*/map.grid.nv, /*sz=*/map.grid.nw, /*connectivity=*/26, /*N=*/N );
   cout << N << endl;
 
-  
-  // Work with IFY
-  // For each label count charaterize the connection directions (a,b,c)
-  // OD channels can be removed => pockets
+  // if channel_dimensions is a null char, it is a pocket  
+  // (AEI time = 16ms)
   string channel_dimensions[N+1]; 
-  for (uint16_t label=1; label<=N; label++) {
-    channel_dimensions[label] += channel_dim(channel_cc_labels, label, map.grid.nu, map.grid.nv, map.grid.nw);
+  for (uint16_t label=1; label<=N; label++) { 
+    channel_dimensions[label] = channel_dim(channel_cc_labels, label, map.grid.nu, map.grid.nv, map.grid.nw);
     cout << channel_dimensions[label] << endl;
+  }
+  // TODO MAKE 2D array for label indexes to use instead of the grid of labels > then we can replace the previous approach by using the labels instead
+  for (size_t i=0; i<array_size; i++){ 
+
   }
 
   // remove symmetrical equivalent channels and count them (for the probability calculation)
   // Work with PSI or KAXQIL
+  // First approach
+  for (uint16_t label_1=1; label_1<N; label_1++) { 
+    if (channel_dimensions[label_1] != "") {
+      for (uint16_t label_2=label_1+1; label_2<=N; label_2++) {
+        if (channel_dimensions[label_2] != "" && 
+            channel_dimensions[label_2].length() == channel_dimensions[label_1].length()) {
+          // cout << label_1 << " could be equivalent to " << label_2 << endl;
+          // function to check if a N randomly selected points of label1 have images in label2
+        }
+      }
+    }
+  }
 
-  // TODO loop over the unique labels and merge them according to pbc (in future change the code to include pbc)
+  // Other approach
+  // Have an array of sorted index per label for non pocket
+  // Make symmetry easily using the grid ops and check if in other label
+
+
 
   // could be done in the code by applying pbc
   // Calculate diffusion coefficients
